7a3fb484e279ef99186c2e82c4c3e8f1fccd0219
Lucas Bernardez <genomarpg@gmail.com>
2024-10-28 01:07:16 -0300

    test
    test

--- Includes/header.php
+++ Includes/header.php
@@ -22,7 +22,7 @@
   <nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">
-        <img src="../../TP2_Allario_Bernardez_Ruiz/logo/logo.png" alt="">
+        <img class="logonav" src="../../TP2_Allario_Bernardez_Ruiz/logo/logo.png" alt="">
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
