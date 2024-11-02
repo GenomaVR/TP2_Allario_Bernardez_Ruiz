7eb95a8f38add95dd1bc5f28413bcc54516cfc78
Lucas Bernardez <genomarpg@gmail.com>
2024-11-02 19:53:29 -0300

    update
    nav y footer

--- Includes/footer.php
+++ Includes/footer.php
@@ -1,35 +1,83 @@
-<?php 
+<?php
 // Aquí puedes agregar lógica PHP 
 ?>
 
+<footer class="footer">
+  <!-- Parte superior del footer -->
+  <div class="footer-subscribe">
+    <div class="subscribe-text-container">
+      <p class="subscribe-text mb-0">
+        <span class="small-text">SUBSCRIBITE AL</span>
+        <span class="large-text">NEWSLETTER</span>
+      </p>
+    </div>
+    <div class="subscribe-mail">
+      <form class="d-flex subscribe-bar" role="search">
+        <input class="form-control subscribe-input me-2" type="email" placeholder="Ingresa tu email" aria-label="Email" />
+        <button class="btn subscribe-btn" type="submit">
+          <i class="bi bi-search"></i>
+        </button>
+      </form>
+    </div>
+  </div>
 
-<footer class="py-3 my-4">
-      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+  <!-- Parte inferior del footer -->
+  <div class="footer-links d-flex justify-content-start">
+    <div class="link-column">
+      <p class="links-title">NUESTROS LINKS</p>
+      <ul class="nav flex-column" style="list-style-type: none; padding: 0;">
         <li class="nav-item">
           <a href="#" class="nav-link px-2">Home</a>
         </li>
         <li class="nav-item">
-          <a href="#" class="nav-link px-2">Features</a>
+          <a href="#" class="nav-link px-2">Tienda</a>
         </li>
         <li class="nav-item">
-          <a href="#" class="nav-link px-2">Pricing</a>
+          <a href="#" class="nav-link px-2">Nosotros</a>
         </li>
         <li class="nav-item">
-          <a href="#" class="nav-link px-2">FAQs</a>
-        </li>
-        <li class="nav-item">
-          <a href="#" class="nav-link px-2">About</a>
+          <a href="#" class="nav-link px-2">Contacto</a>
         </li>
       </ul>
-      <p class="text-center">© 2024 Company, Inc</p>
-    </footer>
+    </div>
 
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-      crossorigin="anonymous"
-    ></script>
-    
-    </body>
-    </html>
+    <div class="link-column">
+      <p class="links-title">SPONSORS</p>
+      <ul class="nav flex-row" style="list-style-type: none; padding: 0;">
+        <li class="nav-item">
+          <img class="logos" src="./logo/inca.png" alt="INCA">
+        </li>
+        <li class="nav-item">
+          <img class="logos" src="./logo/telefe.png" alt="Telefe">
+        </li>
+        <li class="nav-item">
+          <img class="logos" src="./logo/vorterix.png" alt="INCA">
+        </li>
+        <li class="nav-item">
+          <img class="logos" src="./logo/cinemark.png" alt="INCA">
+        </li>
+      </ul>
+    </div>
+  </div>
 
+
+
+  <div class="d-flex justify-content-between align-items-center">
+    <p class="text-center mb-0" style="color: white;">© 2024 TP PHP allario bernardez ruiz</p>
+    <div class="social-icons">
+      <a href="#" class="me-2 text-white"><i class="bi bi-facebook"></i></a>
+      <a href="#" class="me-2 text-white"><i class="bi bi-twitter"></i></a>
+      <a href="#" class="me-2 text-white"><i class="bi bi-instagram"></i></a>
+    </div>
+  </div>
+</footer>
+
+
+<script
+  src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+  crossorigin="anonymous"></script>
+
+</body>
+
+</html>
--- Includes/header.php
+++ Includes/header.php
@@ -9,12 +9,15 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>El Cinfeilo </title>
-  
+
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="../../TP2_Allario_Bernardez_Ruiz/src/styles.css">
-  
+
 </head>
 
 <body>
@@ -37,7 +40,7 @@
           </li>
 
           <li class="nav-item">
-            <a class="nav-link" href="#">WatchList</a>
+            <a class="nav-link" href="#">Tienda</a>
           </li>
 
           <li class="nav-item">
@@ -48,11 +51,12 @@
           </li>
 
         </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-          <button class="btn btn-outline-success" type="submit">
-            Search
+        <form class="d-flex search-bar" role="search">
+          <input class="form-control search-input me-2" type="search" placeholder="Search" aria-label="Search" />
+          <button class="btn search-btn" type="submit">
+            <i class="bi bi-search"></i>
           </button>
+        </form>
         </form>
       </div>
     </div>
