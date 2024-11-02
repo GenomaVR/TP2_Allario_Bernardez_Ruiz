4a9ba2fc24d6cb4ee7d7adee1b77e70762f42da0
LucasRuiz <168296252+chill0101@users.noreply.github.com>
2024-11-02 13:51:39 -0300

    construccion-sliders

--- Includes/footer.php
+++ Includes/footer.php
@@ -2,26 +2,25 @@
 // Aquí puedes agregar lógica PHP 
 ?>
 
-
 <footer class="py-3 my-4">
-      <ul class="nav justify-content-center border-bottom pb-3 mb-3 text-white">
+      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
         <li class="nav-item">
-          <a href="#" class="nav-link px-2 text-white">Home</a>
+          <a href="#" class="nav-link px-2">Home</a>
         </li>
         <li class="nav-item">
-          <a href="#" class="nav-link px-2 text-white">Features</a>
+          <a href="#" class="nav-link px-2">Features</a>
         </li>
         <li class="nav-item">
-          <a href="#" class="nav-link px-2 text-white">Pricing</a>
+          <a href="#" class="nav-link px-2">Pricing</a>
         </li>
         <li class="nav-item">
-          <a href="#" class="nav-link px-2 text-white">FAQs</a>
+          <a href="#" class="nav-link px-2">FAQs</a>
         </li>
         <li class="nav-item">
-          <a href="#" class="nav-link px-2 text-white">About</a>
+          <a href="#" class="nav-link px-2">About</a>
         </li>
       </ul>
-      <p class="text-center  text-white">© 2024 Company, Inc</p>
+      <p class="text-center">© 2024 Company, Inc</p>
     </footer>
 
     <script
--- index.php
+++ index.php
@@ -47,7 +47,7 @@ include './Includes/header.php';
   <h3>Para ver ahora mismo</h3>
 </div>
 <div id="carouselExample2" class="carousel slide d-flex justify-content-center position-relative m-3" data-bs-ride="carousel">
-    <div class="container">
+    <div class="container-fluid">
         <div class="carousel-inner">
             <div class="carousel-item active">
                 <div class="d-flex justify-content-between">
@@ -78,6 +78,80 @@ include './Includes/header.php';
 </div>
 </div>
 
+
+
+<div class="container-fluid slider-medium-2col my-5 py-5">
+    <div class="container-columns d-flex flex-row md-flex-wrap">
+        <div class="call-to-action d-flex flex-column justify-content-center align-items-cente text-center">
+            <h3 class="text-white">MIRA MAS PERRI</h3>
+            <a href="index.php" class="btn btn-dark">MIRA MAS DIJE</a>
+        </div>
+        <div class="slider-verticalcards container-fluid">
+            <div class="carousel2" id="customCarousel">
+                <div class="carousel-inner2 p-4 gap-5">
+                    <div class="carousel-item2"><img src="https://assets.website-files.com/62cd5c6f4947cd559a9cc4de/62d28d2f683aff27688f2f7e_freeguy-p-500.jpg" width="200" alt=""></div>
+                    <div class="carousel-item2"><img src="https://assets.website-files.com/62cd5c6f4947cd559a9cc4de/62d025ce78d11b309d63b7cf_dollface-p-500.jpg" width="200" alt=""></div>
+                    <div class="carousel-item2"><img src="https://assets.website-files.com/62cd5c6f4947cd559a9cc4de/62d28cd5683aff357f8f2b13_pistol-p-500.jpg" width="200" alt=""></div>
+                    <div class="carousel-item2"><img src="https://assets.website-files.com/62cd5c6f4947cd559a9cc4de/62d025b5a482d939fd8813c5_prisonbreak-p-500.jpg" width="200" alt=""></div>
+                    <div class="carousel-item2"><img src="https://assets.website-files.com/62cd5c6f4947cd559a9cc4de/62d025b5a482d939fd8813c5_prisonbreak-p-500.jpg" width="200" alt=""></div>
+                    <div class="carousel-item2"><img src="https://assets.website-files.com/62cd5c6f4947cd559a9cc4de/62d025ce78d11b309d63b7cf_dollface-p-500.jpg" width="200" alt=""></div>
+                    <div class="carousel-item2"><img src="https://assets.website-files.com/62cd5c6f4947cd559a9cc4de/62d28cd5683aff357f8f2b13_pistol-p-500.jpg"  width="200" alt=""></div>
+                    <div class="carousel-item2"><img src="https://assets.website-files.com/62cd5c6f4947cd559a9cc4de/62d025b5a482d939fd8813c5_prisonbreak-p-500.jpg" width="200" alt=""></div>
+                    <div class="carousel-item2"><img src="https://assets.website-files.com/62cd5c6f4947cd559a9cc4de/62d025ce78d11b309d63b7cf_dollface-p-500.jpg" width="200" alt=""></div>
+                    <div class="carousel-item2"><img src="https://assets.website-files.com/62cd5c6f4947cd559a9cc4de/62d28cd5683aff357f8f2b13_pistol-p-500.jpg"  width="200" alt=""></div>
+                    <div class="carousel-item2"><img src="https://assets.website-files.com/62cd5c6f4947cd559a9cc4de/62d025b5a482d939fd8813c5_prisonbreak-p-500.jpg" width="200" alt=""></div>
+                </div>
+                <button class="carousel-control-prev" id="prevBtn">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>  
+                </button>
+                <button class="carousel-control-next" id="nextBtn">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
 <?php 
 include './Includes/footer.php';
 ?>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+    const carousel = document.querySelector('#customCarousel .carousel-inner2');
+    const prevBtn = document.getElementById('prevBtn');
+    const nextBtn = document.getElementById('nextBtn');
+    let currentIndex = 3;
+    const itemsPerSlide = 4;
+    const totalItems = document.querySelectorAll('#customCarousel .carousel-item2').length;
+
+    function updateCarousel() {
+        const offset = -currentIndex * (100 / itemsPerSlide);
+        carousel.style.transform = `translateX(${offset}%)`;
+    }
+
+    prevBtn.addEventListener('click', function () {
+        if (currentIndex > 0) {
+            currentIndex--;
+        } else {
+            currentIndex = totalItems - itemsPerSlide;
+        }
+        updateCarousel();
+    });
+
+    nextBtn.addEventListener('click', function () {
+        if (currentIndex < totalItems - itemsPerSlide) {
+            currentIndex++;
+        } else {
+            currentIndex = 0;
+        }
+        updateCarousel();
+    });
+
+    updateCarousel();
+});
+</script>
