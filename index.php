fe03b54cfe86b9396cf267636ce1499600befa32
LucasRuiz <168296252+chill0101@users.noreply.github.com>
2024-11-08 19:46:50 -0300

    commit 08-11

--- index.php
+++ index.php
@@ -200,6 +200,83 @@ include './Includes/header.php';
 
 
 
+<div class="container mt-5 text-white">
+  <h3>Para ver ahora mismo</h3>
+</div>
+<div id="carouselExample4" class="container carousel slide d-flex justify-content-center position-relative" data-bs-ride="carousel">
+    <div class="d-flex justify-content-center">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <div class="d-flex justify-content-center gap-3 card-rows row">
+        <div class="card p-2 border-4 bg-light main-cards col-2">
+            <div class="img-box">
+                <img src="https://assets.website-files.com/62cd5c6f4947cd559a9cc4de/62cd64e5e1c3b915971ba8f8_marvel_logo.png" alt="">
+            </div>
+        </div>
+        <div class="card p-2 border-4 bg-light main-cards col-2">
+            <div class="img-box">
+                <img src="https://assets.website-files.com/62cd5c6f4947cd559a9cc4de/62cd64e5e1c3b915971ba8f8_marvel_logo.png" alt="">
+            </div>
+        </div>
+        <div class="card p-2 border-4 bg-light main-cards col-2">
+            <div class="img-box">
+                <img src="https://assets.website-files.com/62cd5c6f4947cd559a9cc4de/62cd64e5e1c3b915971ba8f8_marvel_logo.png" alt="">
+            </div>
+        </div>
+        <div class="card p-2 border-4 bg-light main-cards col-2">
+            <div class="img-box">
+                <img src="https://assets.website-files.com/62cd5c6f4947cd559a9cc4de/62cd64e5e1c3b915971ba8f8_marvel_logo.png" alt="">
+            </div>
+        </div>
+        <div class="card p-2 border-4 bg-light main-cards col-2">
+            <div class="img-box">
+                <img src="https://assets.website-files.com/62cd5c6f4947cd559a9cc4de/62cd64e5e1c3b915971ba8f8_marvel_logo.png" alt="">
+            </div>
+        </div>
+    </div>
+            </div>
+            <div class="carousel-item">
+            <div class="d-flex justify-content-center gap-3 card-rows row">
+        <div class="card p-2 border-4 bg-light main-cards col-2">
+            <div class="img-box">
+                <img src="https://assets.website-files.com/62cd5c6f4947cd559a9cc4de/62cd64e5e1c3b915971ba8f8_marvel_logo.png" alt="">
+            </div>
+        </div>
+        <div class="card p-2 border-4 bg-light main-cards col-2">
+            <div class="img-box">
+                <img src="https://assets.website-files.com/62cd5c6f4947cd559a9cc4de/62cd64e5e1c3b915971ba8f8_marvel_logo.png" alt="">
+            </div>
+        </div>
+        <div class="card p-2 border-4 bg-light main-cards col-2">
+            <div class="img-box">
+                <img src="https://assets.website-files.com/62cd5c6f4947cd559a9cc4de/62cd64e5e1c3b915971ba8f8_marvel_logo.png" alt="">
+            </div>
+        </div>
+        <div class="card p-2 border-4 bg-light main-cards col-2">
+            <div class="img-box">
+                <img src="https://assets.website-files.com/62cd5c6f4947cd559a9cc4de/62cd64e5e1c3b915971ba8f8_marvel_logo.png" alt="">
+            </div>
+        </div>
+        <div class="card p-2 border-4 bg-light main-cards col-2">
+            <div class="img-box">
+                <img src="https://assets.website-files.com/62cd5c6f4947cd559a9cc4de/62cd64e5e1c3b915971ba8f8_marvel_logo.png" alt="">
+            </div>
+        </div>
+    </div>
+            </div>
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample4" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample4" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+</div>
+
 
 
 <?php 
